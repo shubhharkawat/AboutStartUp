8f79b5105f4b9bd07115d6098a272d98eadab985
shubhharkawat <shubhharkawat1406@gmail.com>
2021-12-06 14:03:33 +0530

    About Start-Up Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./Header"
+import Summary from './Summary'
+import Team from './Team'
+import Financials from './Financials'
+// import Business from './Business'
+// import Products from './Products'
+// import Requirements from './Requirements'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        < Header />
+      </div>
+      {/* <Summary /> */}
+      <Routes>
+        <Route path="/" elements={<Summary />}/>
+        <Route path="/Team" elements={<Team />}/>
+        <Route path="/Financials" elements={<Financials />}/>
+        {/* <Route path="/Business" elements={<Business />} />
+        <Route path="/Products" elements={<Products />} />
+        <Route path="/Requirements" elements={<Requirements />} /> */}
+      </Routes>
+    </BrowserRouter>
+   
   );
 }
 
